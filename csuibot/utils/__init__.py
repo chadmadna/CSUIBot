709a64a9b54d717e8d02a54d5ae6abcc9f024cf1
Amelia Rahastiyani <amelia.alpherg@gmail.com>
2016-05-24 16:06:20 +0700

    bot testing 2

--- csuibot/utils/__init__.py
+++ csuibot/utils/__init__.py
@@ -35,4 +35,5 @@ def lookup_chinese_zodiac(year):
 def lookup_hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    result = tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    return str(result)
